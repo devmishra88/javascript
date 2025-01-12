5b470b06bd4e958de3562e3c1f26d2659a91d967
devmishra88 <devmishra88@gmail.com>
2025-01-12 22:04:06 +0530

    javascript variables use case comment added

--- javascript_variables.js
+++ javascript_variables.js
@@ -1,3 +1,8 @@
+/*
+Prefer not to use var
+because of issue in block scope and functional scop
+*/
+
 const accountId = 123456;
 let accountEmail = "deveshmishra@live.in";
 var accountPassword = "demo";
