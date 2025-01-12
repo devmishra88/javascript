bfbcae51fbc7e34d424f1bbe21d5e2a368cce689
devmishra88 <devmishra88@gmail.com>
2025-01-12 22:28:54 +0530

    datatype intro

--- datatype.js
+++ datatype.js
@@ -2,4 +2,32 @@
 
 //alert(3+3); //we are using node js, not browser so this alert will not work
 
+console.log(3 + 3);
+
+console.log("Devesh");
+
+let name = "Devesh" //string
+let age = 34; //Number
+let isLoggedIn = true; //boolean
+
+
+//data type
+
+//premitive data type
+
+// number = 2 to the power 53
+// bigint
+// string = ""
+// boolean = true/false
+// null = standalone value (represention of empty value)
+// undefined = a variable is declear but value is not defined
+// symbol = to find uniqness mostly used
+// objects =  
+// 
+
+console.log(typeof "Devesh");
+
+console.log(typeof null); //this will return object
+
+console.log(typeof undefined); //this will return undefined
 
