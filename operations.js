5ceb5a56706e8805e2568497f6e0feeaabc8b90d
devmishra88 <devmishra88@gmail.com>
2025-01-24 23:35:39 +0530

    comprision operations added

---
+++ operations.js
@@ -0,0 +1,59 @@
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+
+// console.log(someNumber);
+// console.log(typeof stringNumber);
+
+// **********************Operations*******************************
+
+let value = 3
+let negativeValue = -value
+// console.log(negativeValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(3**3); //power operation 3 to the power 3
+// console.log(2/2);
+// console.log(11%6);
+
+// let str1 = "hello"
+// let str2 = " Devesh"
+// let str3 = str1 + str2
+
+// console.log(str3)
+// console.log("1"+2);
+
+// console.log(3 + 4 * 5 % 3); //Answer will be 5
+
+// console.log(+true); // output will 1
+// console.log(+false); // output will 0
+// console.log(true+); // will through error
+
+let num1, num2, num3;
+
+// num1 = num2 = num3 = 2+2 //output will 4
+
+// console.log(num1);
+// console.log(num2);
+// console.log(num3);
+
+let gameCouner = 100;
+++gameCouner;
+// console.log(gameCouner);
+
+//Prefix and Postfix operators
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
+
+// console.log("2" > 1); //true
+// console.log("02" > 1); //true
+
+// console.log(null > 0); //false
+// console.log(null == 0); //false
+// console.log(null >= 0); //true
+
+console.log(undefined > 0); //false
+console.log(undefined == 0); //false
+console.log(undefined >= 0); //false
