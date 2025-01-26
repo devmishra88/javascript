8de3692a4a76bcb5288be400ea57f331cc42faef
devmishra88 <devmishra88@gmail.com>
2025-01-26 22:06:32 +0530

    datatype summary study

---
+++ datatype_summary.js
@@ -0,0 +1,38 @@
+// Primitive Data Type
+
+// 7 Types : String, Number, Boolean, null (0 not empty), undefined, Symbol, BigInt
+
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outSideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const secondId = Symbol('123')
+
+console.log(id)
+console.log(secondId)
+
+console.log(id === secondId) //this will return false
+
+const bigNumber = 12345678912345678n //big int number
+
+//JavaScript is a dynamically typed language, which means that data types of variables are determined by the value they hold at runtime and can change throughout the program as we assign different values to them.
+
+// Reference Type (Non Primitive Data Type)
+
+// Array, Objects, Functions
+
+const heros = ["nagraj", "scd", "doga"]
+const objects = {
+    name: "Devesh",
+    age:34
+}
+
+const myFunction = function(){
+    console.log("Hello word");    
+}
+
